b52d818db3d9ed5fef2165c480ffded2c426d3f4
AdamSJohnson <Adam.Johnson.main@gmail.com>
2017-03-04 21:36:30 -0500

    Edited the azapi.py to work with azlyrics.az

--- azapi.py
+++ azapi.py
@@ -24,22 +24,27 @@ from bs4 import BeautifulSoup
 import urllib.request, urllib.error, urllib.parse
 
 def generating(artist, title, save):
-        artist = artist.lower().replace(" ", "%20")
-        title = title.lower().replace(" ", "%20")
-        generate_url = 'http://azlyrics.com/lyrics/'+artist+'/'+title +'.html'
+        artist = artist.lower().replace(" ", "-")
+        title = title.lower().replace(" ", "-")
+        generate_url = 'http://azlyrics.az/'+artist+'/-/'+title +'.html'
+        print(generate_url)
         processing(generate_url, artist, title, save)
         
 def processing(generate_url, artist, title, save):
-    response = urllib.request.urlopen(generate_url)
+    first = urllib.request.Request(generate_url, headers ={'User-agent' : 'Magic Browser'})
+    response = urllib.request.urlopen(first)
     read_lyrics = response.read()
-    soup = BeautifulSoup(read_lyrics)
+
+    soup = BeautifulSoup(read_lyrics, "html.parser")
+    print(soup)
     lyrics = soup.find_all("div", attrs={"class": None, "id": None})
+    print(lyrics)
     lyrics = [x.getText() for x in lyrics]
     printing(artist, title, save, lyrics)
     
 def printing(artist, title, save, lyrics):    
-    for x in lyrics:
-        print(x, end="\n\n")
+    #for x in lyrics:
+    #    print(x, end="\n\n")
     if save == True:
         saving(artist, title, lyrics)
     elif save == False:
--- pynn.py
+++ pynn.py
@@ -3,6 +3,7 @@ from nltk.corpus import sentiwordnet as swn
 from nltk.corpus import treebank as tb
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from os import path
+import azapi
 
 class analyze():
 
@@ -10,21 +11,57 @@ class analyze():
     Start analyzing the words
     '''
     def setup_analysis(self):
-        fname = 'DMX_test.txt'
+        #get the file name
+
+
+        azapi.generating('katy perry', 'roar', True)
+        fname = 'katy-perry_roar.txt'
+
+        #veriffy we got the right name
         print(fname)
+
+        #open the file
         file_obj = open(fname, 'r')
+
+        #put an array of the lines in a variable
         sentences = file_obj.readlines()
+
+        #setup an intensities array
         intensities = []
+
+        #keep track of the lines processed
         lines = 0
+
+        #go through an analyze each of the sentences
         for sentence in sentences:
+            if sentence == '\n':
+                continue
+
+            if sentence.startswith('['):
+                continue
+
             lines += 1
             sid = SentimentIntensityAnalyzer()
             ss = sid.polarity_scores(sentence)
             print(sentence)
+            temp = []
             for k in sorted(ss):
-                intensities.append(k)
-                print('{0}: {1}, '.format(k, ss[k]), end='')
-            print('\n')
+                j = ss[k]
+                temp.append(j)
+            intensities.append(temp)
+
+        total = [0.0, 0.0, 0.0, 0.0]
+        for intensity in intensities:
+            total[0] += intensity[0]
+            total[1] += intensity[1]
+            total[2] += intensity[2]
+            total[3] += intensity[3]
+
+        total[0] = total[0] / lines
+        total[1] = total[1] / lines
+        total[2] = total[2] / lines
+        total[3] = total[3] / lines
+        print(total)
 
 a = analyze()
 a.setup_analysis()
