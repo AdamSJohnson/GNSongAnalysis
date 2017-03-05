74b4a6064193588b9e2c3667a5ad126972886164
Garryg <griggsgs190@potsdam.edu>
2017-03-05 02:22:08 -0500

    maped out values

--- pygui.py
+++ pygui.py
@@ -62,9 +62,11 @@ class ComboBoxWindow(Gtk.Window):
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
 
         genre_store = Gtk.ListStore(str)
-        genres = {"Austria", "Brazil", "Belgium", "France", "Germany",
-            "Switzerland", "United Kingdom", "United States of America",
-            "Uruguay"}
+        genres = {"Latin":25982, "Blues":36060, "Classical":36061, "World":25984, "Electronica":36055,
+            "Country & Western":36059, "Comedy, Spoken & Other":36064, "Traditional Pop":25978,
+            "Fold":25971, "Punk":36051, "New Age":36062, "Rock":25964, "Soul/R&B":36057, "Soundtrack":36063, "Reggae":36065,
+            "Gospel & Christian":25976, "Children's":25980, "Alternative":25961, "Metal":36053, "Rap/Hip-Hop":36058,
+            "Pop":36056, "Dance & House":36054, "Oldies":25965, "Jazz":25974, "Indie":36052}
         for genre in genres:
             genre_store.append([genre])
 
@@ -76,9 +78,11 @@ class ComboBoxWindow(Gtk.Window):
         vbox.pack_start(genre_combo, False, False, True)
 
         mood_store = Gtk.ListStore(str)
-        moods = ["Austria", "Brazil", "Belgium", "France", "Germany",
-            "Switzerland", "United Kingdom", "United States of America",
-            "Uruguay"]
+        moods = {"Peaceful":65322, "Romantic":65323, "Sentimental":65324, "Tender":42946, "Easygoing":42946,
+            "Yearning":65325, "Sophisticated":42954, "Sensual":42947,
+            "Cool":65326, "Gritty":65327, "Somber":42948, "Melancholy":42949, "Serious":65328, "Brooding":65329, "Fiery":42953,
+            "Urgent":42955, "Defiant":42951, "Aggressive":42958, "Rowdy":65330, "Excited":42960,
+            "Energizing":42961, "Energizing":42945, "Stirring":65331, "Lively":65332, "Upbeat":65333}
         for mood in moods:
             mood_store.append([mood])
 
@@ -90,9 +94,7 @@ class ComboBoxWindow(Gtk.Window):
         vbox.pack_start(mood_combo, False, False, True)
 
         era_store = Gtk.ListStore(str)
-        eras = ["Austria", "Brazil", "Belgium", "France", "Germany",
-            "Switzerland", "United Kingdom", "United States of America",
-            "Uruguay"]
+        eras = {"1930":29490, "1940":29489, "1950":29488, "1960":29487, "1970":29486, "1980":29485, "1990":29484, "2000":29483, "2010":42877}
         for era in eras:
             era_store.append([era])
 
