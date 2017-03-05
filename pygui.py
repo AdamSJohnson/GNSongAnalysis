a6c227f2362d249719907bfa83bc93e21ba24e36
Mike <mike42092@gmail.com>
2017-03-05 07:09:13 -0500

    pygui.py gathers data and stores into an array. Drop downs do not lock in the selection, give critical on choosing, but run fine and still give correct data.

--- pygui.py
+++ pygui.py
@@ -82,12 +82,12 @@ class ComboBoxWindow(Gtk.Window):
         #Combo boxes have choices, or user data entry
         #Indivual genres are stored and selectable
         genre_store = Gtk.ListStore(str)
-        genres = {"Latin":25982, "Blues":36060, "Classical":36061, "World":25984, "Electronica":36055,
+        self.genres = {"Latin":25982, "Blues":36060, "Classical":36061, "World":25984, "Electronica":36055,
             "Country & Western":36059, "Comedy, Spoken & Other":36064, "Traditional Pop":25978,
             "Fold":25971, "Punk":36051, "New Age":36062, "Rock":25964, "Soul/R&B":36057, "Soundtrack":36063, "Reggae":36065,
             "Gospel & Christian":25976, "Children's":25980, "Alternative":25961, "Metal":36053, "Rap/Hip-Hop":36058,
             "Pop":36056, "Dance & House":36054, "Oldies":25965, "Jazz":25974, "Indie":36052}
-        for genre in genres:
+        for genre in self.genres:
             genre_store.append([genre])
 
         genre_combo = Gtk.ComboBox.new_with_model_and_entry(genre_store)
@@ -99,12 +99,12 @@ class ComboBoxWindow(Gtk.Window):
 
         #Indivual moods are stored and selectable
         mood_store = Gtk.ListStore(str)
-        moods = {"Peaceful":65322, "Romantic":65323, "Sentimental":65324, "Tender":42946, "Easygoing":42946,
+        self.moods = {"Peaceful":65322, "Romantic":65323, "Sentimental":65324, "Tender":42946, "Easygoing":42946,
             "Yearning":65325, "Sophisticated":42954, "Sensual":42947,
             "Cool":65326, "Gritty":65327, "Somber":42948, "Melancholy":42949, "Serious":65328, "Brooding":65329, "Fiery":42953,
             "Urgent":42955, "Defiant":42951, "Aggressive":42958, "Rowdy":65330, "Excited":42960,
             "Energizing":42961, "Energizing":42945, "Stirring":65331, "Lively":65332, "Upbeat":65333}
-        for mood in moods:
+        for mood in self.moods:
             mood_store.append([mood])
 
         mood_combo = Gtk.ComboBox.new_with_model_and_entry(mood_store)
@@ -116,8 +116,8 @@ class ComboBoxWindow(Gtk.Window):
 
         #Indivual eras are stored and selectable
         era_store = Gtk.ListStore(str)
-        eras = {"1930":29490, "1940":29489, "1950":29488, "1960":29487, "1970":29486, "1980":29485, "1990":29484, "2000":29483, "2010":42877}
-        for era in eras:
+        self.eras = {"1930":29490, "1940":29489, "1950":29488, "1960":29487, "1970":29486, "1980":29485, "1990":29484, "2000":29483, "2010":42877}
+        for era in self.eras:
             era_store.append([era])
 
         era_combo = Gtk.ComboBox.new_with_model_and_entry(era_store)
@@ -139,7 +139,8 @@ class ComboBoxWindow(Gtk.Window):
         if tree_iter != None:
             model = combo.get_model()
             genre = model[tree_iter][0]
-            print("Selected: genre=%s" % genre)
+            self.info[2] = self.genres[genre]
+            #print("Selected: genre=%s" % genre)
 
 
     def on_mood_combo_changed(self, combo):
@@ -147,22 +148,22 @@ class ComboBoxWindow(Gtk.Window):
         if tree_iter != None:
             model = combo.get_model()
             mood = model[tree_iter][0]
-            print("Selected: mood=%s" % mood)
+            self.info[3] = self.moods[mood]
+            #print("Selected: mood=%s" % mood)
 
     def on_era_combo_changed(self, combo):
         tree_iter = combo.get_active_iter()
         if tree_iter != None:
             model = combo.get_model()
             era = model[tree_iter][0]
-            print("Selected: era=%s" % era)
+            self.info[4] = self.eras[era]
+            #print("Selected: era=%s" % era)
 
     def update_info(self):
         self.info[0] = self.artist.get_text()
         self.info[1] = self.track.get_text()
-        for w in self.info
-        print w
-        #print(self.info[0])
-        #print(self.info[1])
+        for w in self.info:
+            print(w)        
 
     def on_click_me_clicked(self, button):
         self.update_info() 
